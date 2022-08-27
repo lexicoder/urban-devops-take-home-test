0ba32d1afe31eed2162a8895af70df0a8c50085b
Samuel Isola <dimeji.isola@gmail.com>
2022-08-27 01:36:47 +0100

    update

--- terraform/shared.tf
+++ terraform/shared.tf
@@ -1,1 +1,7 @@
 data "google_client_config" "provider" {}
+
+resource "google_artifact_registry_repository" "registry" {
+  location      = var.region
+  repository_id = "urban-devops-test"
+  format        = "DOCKER"
+}
