cbd15277410d2873e792a44db4d00ac11fd61a2e
Samuel Isola <dimeji.isola@gmail.com>
2022-08-28 01:28:02 +0100

    update

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,3 +2,9 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+
+provider "kubernetes" {
+  host                   = "https://${module.gke_cluster.endpoint}"
+  token                  = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(module.gke_cluster.cluster_ca_certificate)
+}
